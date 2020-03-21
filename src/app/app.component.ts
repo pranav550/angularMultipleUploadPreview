7a3e0e7add9bb54d6d2159b64f64b43caa417afd
pranav550 <pranavverma550@gmail.com>
2020-03-21 09:41:23 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angualar1';
+  urls: any = []
+  onSelectFile(event) {
+    console.log(event.target.files)
+    if (event.target.files) {
+      for (let i = 0; i < event.target.files.length; i++) {
+        var reader = new FileReader();
+        reader.readAsDataURL(event.target.files[i])
+        reader.onload = (event: any) => {
+          this.urls.push(event.target.result)
+        }
+      }
+    }
+  }
+
 }
